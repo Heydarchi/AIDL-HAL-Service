31e05a44904559976b3b787238c56d90e58f3e4b
Mohammad Hossein Heydarchi <mohammad.heydarchi@volvocars.com>
2022-11-20 15:41:55 +0100

    Implement AIDL HAL service (dummy-service) Adjust build environment for AIDL interface & AIDL service

--- aospinsight/device/devboard/aospinsight_car.mk
+++ aospinsight/device/devboard/aospinsight_car.mk
@@ -4,6 +4,11 @@ $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
 #$(call inherit-product, vendor/recop/device/recop_emulator/device_recop_car.mk)
 
+
+##### Specific BoardConfig #####
+VCC_FEATURES_BOARD_CONFIG_APPENDS += \
+    BoardConfig.mk \
+
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := aosp_car_x86_64
 PRODUCT_DEVICE := generic_car_x86_64
@@ -11,4 +16,7 @@ PRODUCT_BRAND := AOSPInsight
 PRODUCT_MODEL := AOSPInsight Car on x86_64 emulator
 
 EMULATOR_VENDOR_NO_SENSORS := true
+
+PRODUCT_PACKAGES += \
+	\dummy-service
 
