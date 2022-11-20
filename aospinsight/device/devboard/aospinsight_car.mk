e06e2dc93a517c59c5a5ee69c8057b66c71feb9f
Mohammad Hossein Heydarchi <mohammad.heydarchi@volvocars.com>
2022-11-20 19:33:05 +0100

    Add required build configs

--- aospinsight/device/devboard/aospinsight_car.mk
+++ aospinsight/device/devboard/aospinsight_car.mk
@@ -6,8 +6,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
 
 
 ##### Specific BoardConfig #####
-VCC_FEATURES_BOARD_CONFIG_APPENDS += \
-    BoardConfig.mk \
+#VCC_FEATURES_BOARD_CONFIG_APPENDS += \
+#    BoardConfig.mk \
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := aosp_car_x86_64
@@ -17,6 +17,16 @@ PRODUCT_MODEL := AOSPInsight Car on x86_64 emulator
 
 EMULATOR_VENDOR_NO_SENSORS := true
 
-PRODUCT_PACKAGES += \
-	\dummy-service
+########## Device Manifest & Framework Compatibility Matrix Manifest ##########
 
+VENDOR_PATH = vendor/aospinsight
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	$(VENDOR_PATH)/hardware/dummy_service/manifest/dummy_framework_compatibility_matrix.xml \
+
+#DEVICE_MANIFEST_FILE += \
+#	$(VENDOR_PATH)/hardware/dummy_service/manifest/dummy_device_manifest.xml \
+
+PRODUCT_PACKAGES += \
+	dummy-service \
+    GalleryReCop \
