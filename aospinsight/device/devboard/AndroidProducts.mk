8745d1fc3b956f5962abc745fa6f9ce0f587286b
Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>
2022-11-23 23:10:39 +0100

    Add an AIDL interface package to AOSP being available for HAL & application layer (#1)
    * Add an AIDL interface package to AOSP being available for HAL & application layer
    
    Co-authored-by: Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>

---
+++ aospinsight/device/devboard/AndroidProducts.mk
@@ -0,0 +1,7 @@
+COMMON_LUNCH_CHOICES += \
+    aospinsight_car-userdebug \
+
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aospinsight_car.mk \
+
---
+++ aospinsight/device/devboard/aospinsight_car.mk
@@ -0,0 +1,14 @@
+PRODUCT_PACKAGE_OVERLAYS := vendor/recop/device/recop_emulator/common/overlay
+
+$(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
+#$(call inherit-product, vendor/recop/device/recop_emulator/device_recop_car.mk)
+
+EMULATOR_VENDOR_NO_SOUND := true
+PRODUCT_NAME := aosp_car_x86_64
+PRODUCT_DEVICE := generic_car_x86_64
+PRODUCT_BRAND := AOSPInsight
+PRODUCT_MODEL := AOSPInsight Car on x86_64 emulator
+
+EMULATOR_VENDOR_NO_SENSORS := true
+
