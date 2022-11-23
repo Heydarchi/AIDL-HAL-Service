d912a789a5f26cd1630516afc54d961cc2bf57b9
Mohammad Hossein Heydarchi <mohammad.heydarchi@volvocars.com>
2022-11-23 23:41:00 +0100

    Develop some new features in DummyImpl

--- aospinsight/hardware/dummy_service/src/DummyImpl.cpp
+++ aospinsight/hardware/dummy_service/src/DummyImpl.cpp
@@ -11,14 +11,42 @@ namespace dummy_device {
 ndk::ScopedAStatus DummyImpl::getPacketByCallback(const std::shared_ptr<IDummyCallback>& cb) {
    // LOG(INFO) << "dummy_device setting state for id=" << id << " to color " << std::hex << state.color;
   //      return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    std::unique_lock<std::mutex> lock(dummyMutex);
+
     LOG(INFO) << "getPacketByCallback called!";
-        return ndk::ScopedAStatus::ok();
+
+
+    if(cb==nullptr){
+        return ndk::ScopedAStatus::fromExceptionCodeWithMessage(EX_NULL_POINTER, "Couldn't init the callback since the pointer is null!!");
+    }
+
+    if(dummyCallback==nullptr){
+        dummyCallback = cb;
+    }
+    else{
+        return ndk::ScopedAStatus::fromExceptionCodeWithMessage(EX_NULL_POINTER, "Re-initializing the callback is forbiden!!");
+    }
+
+    //Just to send invoke callback method
+    DummyPacket dummyPacket;
+    dummyPacket.id=202;
+    dummyPacket.msg="A test message from the HAL service";
+    onPacketEvent(&dummyPacket);
+    
+    
+    return ndk::ScopedAStatus::ok();
    // }
 }
 
 ndk::ScopedAStatus DummyImpl::getPacket(DummyPacket* dummyPacket) {
+    std::unique_lock<std::mutex> lock(dummyMutex);
     LOG(INFO) << "getPacket called!";
     return ndk::ScopedAStatus::ok();
+}
+
+void DummyImpl::onPacketEvent(DummyPacket* dummyPacket) {
+    LOG(INFO) << "onNewPacket called!";
+    dummyCallback->onGetPacket(*dummyPacket);
 }
 
 }  // namespace dummy_device
--- aospinsight/hardware/dummy_service/src/include/DummyImpl.h
+++ aospinsight/hardware/dummy_service/src/include/DummyImpl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <aidl/aospinsight/hardware/dummy_device/BnDummy.h>
+#include <mutex>
 
 namespace aidl {
 namespace aospinsight {
@@ -15,6 +16,12 @@ namespace dummy_device {
 class DummyImpl : public BnDummy {
     virtual ndk::ScopedAStatus getPacketByCallback(const std::shared_ptr<IDummyCallback>& cb) override;
     virtual ndk::ScopedAStatus getPacket(DummyPacket* dummyPacket) override;
+
+protected:
+    std::shared_ptr<IDummyCallback> dummyCallback = nullptr;
+    std::mutex dummyMutex;
+
+    void onPacketEvent(DummyPacket* dummyPacket);
 };
 
 }  // namespace dummy_device
