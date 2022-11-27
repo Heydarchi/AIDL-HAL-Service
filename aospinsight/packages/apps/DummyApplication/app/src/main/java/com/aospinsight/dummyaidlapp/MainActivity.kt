8e455798d20c36e9d4b1294de27548d90f2b6619
Mohammad Hossein Heydarchi <mohammad.heydarchi@volvocars.com>
2022-11-28 00:43:25 +0100

    Add AIDL interface code

--- aospinsight/packages/apps/DummyApplication/app/src/main/java/com/aospinsight/dummyaidlapp/MainActivity.kt
+++ aospinsight/packages/apps/DummyApplication/app/src/main/java/com/aospinsight/dummyaidlapp/MainActivity.kt
@@ -5,14 +5,24 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.os.Environment
+import android.os.Binder
+import android.os.IBinder
+import android.os.ServiceManager
+
+import aospinsight.hardware.dummy_device.IDummy
+import aospinsight.hardware.dummy_device.DummyPacket
 
 class MainActivity : AppCompatActivity() {
+
+    private var binder: IBinder? = null
+    private lateinit var iDummy: IDummy
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("isExternalStorageReadOnly", " :" + isExternalStorageReadOnly())
         Log.d("isExternalStorageAvailable", " :" + isExternalStorageAvailable())
-
+        getAidlInterface()
     }
 
     private fun isExternalStorageReadOnly(): Boolean {
@@ -29,5 +39,21 @@ class MainActivity : AppCompatActivity() {
             return true;
         }
         return false;
+    }
+
+    private fun getAidlInterface(){
+        binder = ServiceManager.getService(HAL_AIDL_SERVICE_NAME)
+        binder?.let{
+            iDummy = IDummy.Stub.asInterface(binder)
+            iDummy?.let{
+                Log.d(LOG, "DummyApp is bound to dummy_service")
+                iDummy.getPacket(DummyPacket())
+            }
+        }
+    }
+
+    companion object{
+        val HAL_AIDL_SERVICE_NAME = "aospinsight.hardware.dummy_device.IDummy/default"
+        val LOG = "DummyApp"
     }
 }
