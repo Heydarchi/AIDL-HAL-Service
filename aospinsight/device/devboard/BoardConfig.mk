ed62f80a9affe33a4fba8b17c6bdf588c52492af
Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>
2022-12-06 18:15:46 +0100

    Hal service se policy (#4)
     Add SELinux Policies

--- aospinsight/device/devboard/BoardConfig.mk
+++ aospinsight/device/devboard/BoardConfig.mk
@@ -1,1 +1,14 @@
 
+BOARD_SEPOLICY_DIRS += \
+		vendor/aospinsight/device/devboard/sepolicy/vendor \
+		vendor/aospinsight/device/devboard/sepolicy/service \
+		vendor/aospinsight/device/devboard/sepolicy/hal \
+		vendor/aospinsight/device/devboard/sepolicy/daemon \
+
+
+########## Device Manifest & Framework Compatibility Matrix Manifest ##########
+
+VENDOR_PATH = vendor/aospinsight
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	$(VENDOR_PATH)/hardware/dummy_service/manifest/dummy_framework_compatibility_matrix.xml \
--- aospinsight/device/devboard/aospinsight_car.mk
+++ aospinsight/device/devboard/aospinsight_car.mk
@@ -4,10 +4,7 @@ $(call inherit-product, device/generic/car/emulator/aosp_car_emulator.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_x86_64.mk)
 #$(call inherit-product, vendor/recop/device/recop_emulator/device_recop_car.mk)
 
-
-##### Specific BoardConfig #####
-#VCC_FEATURES_BOARD_CONFIG_APPENDS += \
-#    BoardConfig.mk \
+include vendor/aospinsight/device/devboard//BoardConfig.mk
 
 EMULATOR_VENDOR_NO_SOUND := true
 PRODUCT_NAME := aosp_car_x86_64
@@ -17,16 +14,5 @@ PRODUCT_MODEL := AOSPInsight Car on x86_64 emulator
 
 EMULATOR_VENDOR_NO_SENSORS := true
 
-########## Device Manifest & Framework Compatibility Matrix Manifest ##########
-
-VENDOR_PATH = vendor/aospinsight
-
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-	$(VENDOR_PATH)/hardware/dummy_service/manifest/dummy_framework_compatibility_matrix.xml \
-
-#DEVICE_MANIFEST_FILE += \
-#	$(VENDOR_PATH)/hardware/dummy_service/manifest/dummy_device_manifest.xml \
-
 PRODUCT_PACKAGES += \
 	dummy-service \
-    GalleryReCop \
